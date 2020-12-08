733b794d302b9a80843931f85d34b8e754b46398
SergioPerera <alu0101394503@ull.edu.es>
2020-12-08 17:56:12 +0000

    Terminada

--- find_first_of/main_find_first_of.cc
+++ find_first_of/main_find_first_of.cc
@@ -4,7 +4,8 @@
 using namespace std;
 
 int main (int argc,char *argv[]){
-
+  
+  cout << "Este programa solicita una palabra y un caracter y devuelve la primera posición en la que está el caracter" << endl;
   Usage (argc, argv);
   string in_word{argv[1]}; 
   string in_char{argv[2]};
